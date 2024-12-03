9af2c8da34a531a8d11567c33ac90a6ddc4a6576
sonu79934 <yogasritha8@gmail.com>
2024-12-03 10:59:06 +0530

    Add files via upload

---
+++ assignmentoperator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class assignmentoperator{
+    public static void main(String args []){
+        //Accepting a string
+       int a=45;
+        System.out.println(a+=46);
+        System.out.println(a-=9);
+        System.out.println(a*=2);
+
+    }
+}
