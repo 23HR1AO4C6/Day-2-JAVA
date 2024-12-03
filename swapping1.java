e6499d49bb395e2aacd6b58a1e66adf7741f4d48
sonu79934 <yogasritha8@gmail.com>
2024-12-03 12:20:11 +0530

    Add files via upload

---
+++ swapping1.java
@@ -0,0 +1,15 @@
+
+
+import java.util.*;
+public class swapping1{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        String res=String.format("After swapping \n boy=%d \n girl=%d" ,a,b);
+        System.out.println(res);
+    }
+}
