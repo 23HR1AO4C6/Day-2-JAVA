f2545124bfd99c102769f886ed7a821b5b3127fd
23HR1AO4C6 <yogasritha8@gmail.com>
2024-12-03 10:31:09 +0530

    Add files via upload

---
+++ arithmaticoperators.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class arithmaticoperators{
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
